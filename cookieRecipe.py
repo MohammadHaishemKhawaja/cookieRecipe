3f0448a7f75d835c6a75b057c7938be61a68cd56
MohammadHaishemKhawaja <108665760+MohammadHaishemKhawaja@users.noreply.github.com>
2022-07-05 06:32:05 +0800

    Create cookieRecipe.py
    This calculates the ingredients needed for the amount of cookies you are making.

---
+++ cookieRecipe.py
@@ -0,0 +1,13 @@
+# How many cookies do you want to make?
+# This recipe makes 48 cookies
+
+x = input("How many cookies do you want to make?")
+
+
+
+
+sugar = (float(x))*1.5
+butter = (float(x))*1
+flour = (float(x))*2.75
+print(f"You need {sugar} cups of sugar, {butter} cups of butter, and {flour} cups of flour")
+                
